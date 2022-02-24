7f05fc02a3d0cb5cace9e5f21db746bb8f4e8b6b
Benjamin Stürz <benni@stuerz.xyz>
2022-02-24 14:31:44 +0100

    help Replaced iostreams with libfmt

--- src/op_help.cpp
+++ src/op_help.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <fmt/core.h>
 #include "cmdline.hpp"
 
 namespace minipkg2::cmdline::operations {
@@ -11,50 +11,44 @@ namespace minipkg2::cmdline::operations {
 
     int help_operation::operator()(const std::vector<std::string>& args) {
         if (args.size() > 1) {
-            std::cout << "Usage: minipkg2 help [operation]\n";
+            fmt::print("Usage: minipkg2 help [operation]\n");
             return 1;
         }
 
         const auto print_opt = [](const option& opt) {
-            const std::size_t len = opt.name.length();
-            std::cout << "  " << opt.name;
             switch (opt.type) {
             case option::ALIAS:
-                std::cout << std::string(30 - len, ' ') << "Alias for " << opt.value << ".\n";
+                fmt::print("  {:30}Alias for {}.\n", opt.name, opt.value);
                 break;
             case option::ARG:
-                std::cout << " <ARG>" << std::string(24 - len, ' ') << opt.description << '\n';
+                fmt::print("  {} <ARG>{:{}}{}\n", opt.name, "", 24 - opt.name.size(), opt.description);
                 break;
             case option::BASIC:
-                std::cout << std::string(30 - len, ' ') << opt.description << '\n';
+                fmt::print("  {:30}{}\n", opt.name, opt.description);
                 break;
             }
         };
 
         if (args.size() == 0) {
-            std::cout << "Micro-Linux Package Manager 2\n\nUsage:\n  minipkg2 [...] <operation> [...]\n\nOperations:\n";
+            fmt::print("Micro-Linux Package Manager 2\n\nUsage:\n  minipkg2 [...] <operation> [...]\n\nOperations:\n");
             for (const auto* op : operation::operations) {
-                std::cout << "  " << op->name << op->usage
-                          << std::string(30 - op->name.length() - op->usage.length(), ' ')
-                          << op->description << '\n';
+                fmt::print("  {}{}{:{}}{}\n", op->name, op->usage, "", 30 - op->name.length() - op->usage.length(), op->description);
             }
-            std::cout << "\nGlobal options:\n";
+            fmt::print("\nGlobal options:\n");
             for (const auto& opt : option::global)
                 print_opt(opt);
         } else {
             const auto& op2 = operation::get_op(args[0]);
 
-            std::cout << "Micro-Linux Package Manager 2\n\nUsage:\n  minipkg2 "
-                      << op2.name << op2.usage
-                      << "\n\nDescription:\n  " << op2.description << '\n';
+            fmt::print("Micro-Linux Package Manager 2\n\nUsage:\n  minipkg2 {}{}\n\nDescription:\n  {}\n", op2.name, op2.usage, op2.description);
 
             if (op2.options.size() != 0) {
-                std::cout << "\nOptions:\n";
+                fmt::print("\nOptions:\n");
                 for (const auto& opt : op2.options)
                     print_opt(opt);
             }
         }
-        std::cout << "\nWritten by Benjamin Stürz <benni@stuerz.xyz>\n";
+        fmt::print("\nWritten by Benjamin Stürz <benni@stuerz.xyz>\n");
         return 0;
     }
 }

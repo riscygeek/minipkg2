f4ddc7dfe6a8897034be8ee81136ce97e5747fb6
Benjamin Stürz <benni@stuerz.xyz>
2022-03-05 22:10:46 +0100

    Removed unused script
    Signed-off-by: Benjamin Stürz <benni@stuerz.xyz>

--- util/set_version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-{ [ -f Makefile ] && [ -f meson.build ]; } || { echo "Must be run in the top-level directory." >&2 && exit 1; }
-
-sed -i "s/^VERSION\s*=.*$/VERSION = $1/g" Makefile
-sed -i "s/^\\(\s*version:\s\\)'*.*',\s*$/\1'$1',/g" meson.build
